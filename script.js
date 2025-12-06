cd510c7dabdd8aef9331c64e2ba39a8874038f5e
Chuckky77 <Chuckmcgill113@gmail.com>
2025-12-06 16:51:17 +0700

    'mm'

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+function changeNewColor() {
+    const color_container = document.getElementById("button")
+
+    new_circle.addEventListener("mouseover", () => {
+        new_circle.style.backgroundColor = "red"
+    })
+
+    new_circle.addEventListener("mouseout", () => {
+        new_circle.style.backgroundColor = "blue"
+    })
+
+    circle_container.appendChild(new_circle)
+}
